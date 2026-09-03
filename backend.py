444f46101b7a86f63e7ffc83923686a2ecd99453
Arpita Paul <arpita20047@gmail.com>
2026-09-03 23:05:30 +0530

    Allow Vercel frontend in CORS

--- backend.py
+++ backend.py
@@ -18,7 +18,7 @@ app.mount(
 # React frontend ko backend access karne dena
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://jhalak-ai.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
